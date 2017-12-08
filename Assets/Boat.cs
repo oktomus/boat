e0690e0d27b9cbdf7996014253731472e6d94eda
Kevin Masson <contact.kevinmasson@gmail.com>
2017-12-08 14:03:11 +0100

    Check for other boats before instancing a new one

--- Assets/Boat.cs
+++ Assets/Boat.cs
@@ -4,22 +4,14 @@ using UnityEngine;
 
 public class Boat : MonoBehaviour {
 
-	private Waver waver;
 
 	// Use this for initialization
 	void Start () {
-		waver = GetComponent<Waver> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//transform.position.y = waver.height (transform.position.x, transform.position.z);
-
-		transform.position = new Vector3 (
-			transform.position.x,
-			waver.Height (transform.position.x, transform.position.z),
-			transform.position.z
-		);
+		
 
 	}
 }
--- Assets/Fleet.cs
+++ Assets/Fleet.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Fleet : MonoBehaviour {
 
 	public GameObject boat;
+	public float minDistance = 2.0f;
 
 	private List<GameObject> boats;
 
+
 	void Start () {
 		boats = new List<GameObject> ();
-		
+		boats.Add(Instantiate(boat, new Vector3(0, 0), new Quaternion()));
 	}
 	
 	void Update () {
@@ -29,8 +31,18 @@ public class Fleet : MonoBehaviour {
 	{
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
-		if (Physics.Raycast(ray, out hit))
+		if (Physics.Raycast (ray, out hit)) {
+			// Check if the point isnt near other boats
+			foreach (GameObject b in boats) {
+				Debug.Log ("Distance is " + Vector3.Distance (b.transform.position, hit.point));
+				if (Vector3.Distance (b.transform.position, hit.point) <= minDistance)
+					return;
+			}
+			// Add it
 			boats.Add(Instantiate(boat, hit.point, new Quaternion()));
+
+		}
+			
 
 	}
 }
